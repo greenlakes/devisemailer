6571c8915f0381f990777b7d5e14bde577f3d22a
Greenlakes <greenlakes2005@gmail.com>
2017-03-16 14:16:39 +0000

    mail fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-default to: "info@example.com"
+default to: "greenlakes2005@gmail.com"
 def contact_email(name, email, message)
 @name = name
 @email = email
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -5,6 +5,6 @@ ActionMailer::Base.smtp_settings = {
 :authentication => :plain,
 :user_name => ENV["SENDGRID_USERNAME"],
 :password => ENV["SENDGRID_PASSWORD"],
-:domain => 'devisemailer1.herokuapp.com',
+:domain => 'heroku.com',
 :enable_starttls_auto => true
 }
