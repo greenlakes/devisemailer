cf725f0d1b6835128996d4c597efd27483b08ca0
Greenlakes <greenlakes2005@gmail.com>
2017-03-16 13:05:51 +0000

    mail form finalise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
   def index
     @articles = Article.last(3)
-    
   end
 
   def contact
+    @contact = Contact.new
   end
+
 
   def about
   end
